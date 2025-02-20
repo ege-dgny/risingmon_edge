a0f7a6317a85bcf97dc7cedb6883ddcbe3db4639
doganay_e <ege.dgny03@gmail.com>
2025-02-20 21:39:29 +0300

    setup

--- edge_stream/setup.sh
+++ edge_stream/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+
 echo "[INFO] Updating package lists..."
 sudo apt update
 
-echo "[INFO] Installing GStreamer and libcamera packages..."
+echo "[INFO] Installing GStreamer, libcamera, and related packages..."
+
 sudo apt install -y \
     gstreamer1.0-tools \
     gstreamer1.0-plugins-base \
@@ -11,11 +13,16 @@ sudo apt install -y \
     gstreamer1.0-plugins-bad \
     gstreamer1.0-plugins-ugly \
     gstreamer1.0-libav \
-    libcamera-apps
+    gstreamer1.0-libcamera \
+    libcamera-apps \
+    libcamera-dev \
+    python3-libcamera \
+    python3-kms++ \
+    python3-picamera2
 
-if [ $? -eq 0 ]; then
-    echo "[INFO] GStreamer installation complete."
-else
-    echo "[ERROR] GStreamer installation encountered an error."
+if [ $? -ne 0 ]; then
+    echo "[ERROR] Installation of packages failed."
     exit 1
 fi
+
+echo "[INFO] GStreamer and libcamera installation complete."
