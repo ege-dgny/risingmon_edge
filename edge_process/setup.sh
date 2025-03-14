a37f0f012be70a5b573e623ce25257c166799c9a
doganay_e <ege.dgny03@gmail.com>
2025-03-14 14:56:20 +0300

    edited setup

--- edge_process/setup.sh
+++ edge_process/setup.sh
@@ -10,10 +10,6 @@
 
 set -e  # Exit immediately if a command exits with a non-zero status
 
-echo "=== Setting device IP to 10.85.61.30 and default gateway to 10.85.61.1 ==="
-sudo ifconfig eth0 10.85.61.30 netmask 255.255.255.0 up
-sudo route add default gw 10.85.61.1
-
 echo "=== Updating and upgrading apt packages ==="
 sudo apt-get update
 sudo apt-get -y upgrade
@@ -31,10 +27,11 @@ sudo apt-get install -y \
     gstreamer1.0-tools gstreamer1.0-plugins-base \
     gstreamer1.0-plugins-good gstreamer1.0-plugins-bad \
     gstreamer1.0-plugins-ugly librga-dev \
-    gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav
+    gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav \
+    git netcat-traditional
 
 echo "=== Upgrading pip ==="
-python3 -m pip install --upgrade pip
+python3 -m pip install --break-system-packages --upgrade pip
 
 echo "=== Installing Python libraries (robotpy, ntcore, numpy) ==="
 python3 -m pip install --break-system-packages --upgrade \
